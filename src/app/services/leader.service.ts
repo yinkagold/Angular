cfc5fb4566e9e349a1a28c69744e5cf7ea3d2bdb
Yinka Gold <yinkagolden@gmail.com>
2018-02-01 14:20:39 +0200

    Promise Part 2,showing delays

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -8,14 +8,26 @@ export class LeaderService {
   constructor() { }
   
   getLeaders(): Promise<Leader[]>{
-	  return Promise.resolve(LEADERS);
+	   return new Promise(resolve => {
+		//simulate server latency with 2 second delay
+		setTimeout(() => resolve(LEADERS), 2000)
+	  });
+		  
   }
 
   getLeader(id: number): Promise<Leader>{
-	  return Promise.resolve(LEADERS.filter((leader) => (leader.id == id))[0]);
+	  return new Promise(resolve => {
+		//simulate server latency with 2 second delay
+		setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id==id))[0]), 2000)
+	  
+  });
   }
   
   getFeaturedLeader(): Promise<Leader>{
-	  return Promise.resolve(LEADERS.filter((leader) => (leader.featured))[0]);
+	 return new Promise(resolve => {
+		//simulate server latency with 2 second delay
+		setTimeout(() => resolve(LEADERS.filter((leader) => (leader.featured))[0]), 2000)
+	
+  });
   }
 }
