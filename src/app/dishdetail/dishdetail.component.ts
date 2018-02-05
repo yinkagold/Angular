399260d49cc30ce3e73300a04e2bf59cdaf4754c
Yinka Gold <yinkagolden@gmail.com>
2018-02-05 14:46:29 +0200

    Assignemt 3 with comment

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
@@ -20,17 +22,51 @@ export class DishdetailComponent implements OnInit {
 	prev: number;
 	next: number;
 	
+	commentForm: FormGroup;
+	comment: Comment;
+	formErrors = {
+		'author': '',
+		'comment': ''
+		
+	};
+	
+	validationMessages = {
+		'author': {
+			'required': 'Name is required.',
+			'minlength': 'Name must be at least 2 characters long',
+			'maxlength': 'Name cannot be more than 25 characters long'
+		},
+		'comment':{
+			'required': 'Comment is required'
+			
+		}
+	};
   constructor(private dishservice: DishService,
 	private route: ActivatedRoute,
-	private location: Location) { }
+	private location: Location, private fb: FormBuilder) { 
+		this.createForm();
+	}
 
   ngOnInit() {
 	   this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params
+		this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
-
+	
+	createForm() {
+	  this.commentForm = this.fb.group({
+		 author: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+		 comment: ['',Validators.required],
+		 rating: 5,
+	  });
+	  
+	  this.commentForm.valueChanges
+	  .subscribe(data => this.onValueChanged(data));
+	  
+	  this.onValueChanged(); // (re)set form validation messages
+  }
+  
   
   setPrevNext(dishId: number){
 	  let index = this.dishIds.indexOf(dishId);
@@ -41,5 +77,36 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
 	  this.location.back();
   }
+  
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+  
+  
+   onSubmit(){
+	  this.comment = this.commentForm.value;
+	  this.comment.date = (new Date()).toISOString();
+      this.dish.comments.push(this.comment);
+	  console.log(this.comment);
+	  this.commentForm.reset({
+		  comment: '',
+		  rating: 5,
+		  author: ''
+		 
+	  });
+   }
+   
 
 }
