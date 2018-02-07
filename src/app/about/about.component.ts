4cd002a406deb1ed9a5f1014301467c2bc00b8d1
Yinka Gold <yinkagolden@gmail.com>
2018-02-07 07:49:38 +0200

    Animations Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { flyInOut, expand } from '../animations/app.animation';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 import 'rxjs/add/observable/of';
@@ -8,8 +8,17 @@ import 'rxjs/add/observable/of';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  host:{
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
+
 export class AboutComponent implements OnInit {
 	
 	leaders: Leader[];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,9 +3,9 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { flyInOut, visibility, expand } from '../animations/app.animation';
 import 'rxjs/add/observable/of';
 
 import 'rxjs/add/operator/switchMap';
@@ -14,20 +14,18 @@ import 'rxjs/add/operator/switchMap';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
 	styleUrls: ['./dishdetail.component.scss'],
-	animations: [
-		trigger('visibility', [
-			state('shown', style({
-				transform: 'scale(1.0)',
-				opacity: 1
-			})),
-			state('hidden', style({
-				transform: 'scale(0.5)',
-				opacity: 0
-			})),
-			transition('* => *', animate('0.5s ease-in-out'))
-		])
-	]
+	host:{
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+		flyInOut(),
+		visibility(),
+		expand()
+  ]
 })
+	
+
 export class DishdetailComponent implements OnInit {
 	
 	dish: Dish;
